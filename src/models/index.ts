4a913ffcca1f9c95bd8d3f14e81e44e792638ce2
Ricardo Campos <rickoliver001@hotmail.com>
2023-12-23 12:20:10 -0300

    :sparkles: feat: Criação da tabela Episodes

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,18 @@
 
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
-// Associações:
+// Category-Course:
 Category.hasMany(Course)
 Course.belongsTo(Category)
 
+// Course-Episode
+Course.hasMany(Episode)
+Episode.belongsTo(Course)
+
 export {
   Category,
-  Course
+  Course,
+  Episode
 }
