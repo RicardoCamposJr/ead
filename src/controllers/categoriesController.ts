88e03691262331952a4c6721dc4f1b29e860c7af
Ricardo Campos <rickoliver001@hotmail.com>
2023-12-27 18:59:38 -0300

    :sparkles: feat: Incluindo paginação de categorias

--- src/controllers/categoriesController.ts
+++ src/controllers/categoriesController.ts
@@ -1,18 +1,16 @@
 import { Request, Response } from 'express'
-import { Category } from '../models'
+import { categoryService } from '../services/categoryService'
+import { getPaginationParams } from '../helpers/getPaginationParams'
 
 export const categoriesController = {
   // Método que vai ser executado ao ser feita uma requisição à API:
   index: async (req: Request, res: Response) => {
+    // Incluindo paginação:
+    const [page, perPage] = getPaginationParams(req.query)
+
     try {
-      // Retorna um array das categorias:
-      const categories = await Category.findAll({
-        // Dentro do .findAll podemos definir quais atributos devem ser retornados e a ordem:
-        attributes: ['id', 'name', 'position'],
-        order: [['position', 'ASC']]
-      })
-      // Resposta à requisição:
-      return res.json(categories)
+      const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
+      return res.json(paginatedCategories)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
