461a764fb7cdcf49e1d77c5cd035f4b421842f54
Ricardo Campos <rickoliver001@hotmail.com>
2023-12-27 19:16:03 -0300

    :sparkles: feat: Obtendo informações de um curso

--- src/models/index.ts
+++ src/models/index.ts
@@ -10,7 +10,7 @@ Category.hasMany(Course, { as: 'courses' })
 Course.belongsTo(Category)
 
 // Course-Episode
-Course.hasMany(Episode)
+Course.hasMany(Episode, { as: 'episodes' })
 Episode.belongsTo(Course)
 
 export {
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@
 
 import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
+import { coursesController } from './controllers/coursesController'
 
 // Instanciando um objeto de Router do Express:
 const router = express.Router()
@@ -11,5 +12,7 @@ Esse método está na pasta controllers. */
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
 
+// Rotas Courses:
+router.get('/courses/:id', coursesController.show)
 
 export { router }
