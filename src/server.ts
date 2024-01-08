5d08474e895ad2a510c7bfe2c6c34e352ebdd269
Ricardo Campos <rickoliver001@hotmail.com>
2024-01-08 16:02:56 -0300

    :sparkles: feat: Adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 import { sequelize } from "./database";
 import { adminJs, adminJsRouter } from "./adminjs";
 import { router } from "./routes";
+import cors from 'cors';
 
 const app = express();
+
+// Utilizando o cors:
+app.use(cors())
 
 // Possibilitando a aplicação a utilizar o JSON nas requisições:
 app.use(express.json()) 
